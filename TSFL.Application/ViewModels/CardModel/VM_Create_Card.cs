6bb29399a083fd35c423c207c66134533a0ff987
ATS <thanhthai2604@gmail.com>
2022-10-03 22:59:01 +0700

    master

---
+++ TSFL.Application/ViewModels/CardModel/VM_Create_Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSFL.Application.ViewModels.CardModel
+{
+    public class VM_Create_Card
+    {
+        public string? Name { get; set; }
+    }
+}
