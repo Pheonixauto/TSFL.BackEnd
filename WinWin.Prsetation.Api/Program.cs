032ab675e151680dda60f7d62e7a68e08ad74fdb
ATS <thanhthai2604@gmail.com>
2022-11-06 21:57:13 +0700

    master

--- WinWin.Prsetation.Api/Program.cs
+++ WinWin.Prsetation.Api/Program.cs
@@ -1,7 +1,17 @@
 using WinWin.Infrastructure.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+                      });
+});
 
 builder.Services.IAddServicePersistance(builder.Configuration);
 builder.Services.IAddDependencyInjection();
@@ -20,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
